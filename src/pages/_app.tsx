b603831e1ebec6e34faaad90447e1faa55cb9d7e
ctdlog <qpflapffhs76@gmail.com>
2023-03-18 15:14:14 +0900

    🔨 add sentry setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import * as Sentry from '@sentry/nextjs'
 import type { AppProps } from 'next/app'
 
 import '@/styles/reset.css'
 import GlobalStyles from '@/styles/GlobalStyles'
+
+Sentry.init({
+  dsn: 'https://3c19309a3dc7445991cd088471e20d5b@o4504858053509120.ingest.sentry.io/4504858080837632',
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
