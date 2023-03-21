fe344a0a6a46803867fa4e9e9a0e1199a7ea9b64
ctdlog <qpflapffhs76@gmail.com>
2023-03-21 23:46:45 +0900

    ✨ add full-page scroll

--- src/components/views/Landing/Landing.styled.tsx
+++ src/components/views/Landing/Landing.styled.tsx
@@ -2,11 +2,22 @@ import styled from '@emotion/styled'
 import tw from 'twin.macro'
 
 const Layout = styled.div`
-  ${tw`flex min-h-screen flex-col`}
+  ${tw`flex flex-col`}
 `
 
-const Main = styled.main`
-  ${tw`mt-24 flex-1`}
+const ScrollBlock = styled.div`
+  ${tw`mt-24 h-screen overflow-y-auto`}
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  section {
+    ${tw`flex h-screen items-center justify-center`}
+
+    h1 {
+      ${tw`text-6xl text-white`}
+    }
+  }
 `
 
-export { Layout, Main }
+export { Layout, ScrollBlock }
--- src/components/views/Landing/LandingContainer.tsx
+++ src/components/views/Landing/LandingContainer.tsx
@@ -1,15 +1,52 @@
+import { useRef, WheelEvent } from 'react'
+
 import Header from '@/components/common/Header'
 
 import * as S from './Landing.styled'
 
 const LandingContainer = () => {
+  const scrollableDivRef = useRef<HTMLDivElement>(null)
+
+  const handleWheel = (e: WheelEvent<HTMLDivElement>) => {
+    const { deltaY } = e
+    const { scrollTop } = scrollableDivRef.current as HTMLDivElement
+    const pageHeight = window.innerHeight
+    const isScrollingDown = deltaY > 0
+    const scrollDirection = isScrollingDown ? 1 : -1
+    const nextPageTop = pageHeight * (Math.floor(scrollTop / pageHeight) + scrollDirection)
+
+    scrollableDivRef.current?.scrollTo({
+      top: nextPageTop,
+      behavior: 'smooth',
+    })
+  }
+
   return (
     <S.Layout>
       <Header />
-      <S.Main>
-        <h1>Smart Auction</h1>
-        <h2>Deploy</h2>
-      </S.Main>
+      <S.ScrollBlock ref={scrollableDivRef} onWheel={handleWheel}>
+        <section
+          style={{
+            background: '#ff8787',
+          }}
+        >
+          <h1>Page 1</h1>
+        </section>
+        <section
+          style={{
+            background: '#9775fa',
+          }}
+        >
+          <h1>Page 2</h1>
+        </section>
+        <section
+          style={{
+            background: '#4dabf7',
+          }}
+        >
+          <h1>Page 3</h1>
+        </section>
+      </S.ScrollBlock>
     </S.Layout>
   )
 }
