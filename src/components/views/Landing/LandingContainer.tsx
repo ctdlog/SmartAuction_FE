37deb112fa4496909c39961f3a7de4bbb8b75f13
ctdlog <qpflapffhs76@gmail.com>
2023-03-23 23:01:23 +0900

    💄 add description page (#2)

--- src/components/views/Landing/LandingContainer.tsx
+++ src/components/views/Landing/LandingContainer.tsx
@@ -1,33 +1,18 @@
-import { useRef, WheelEvent } from 'react'
-
 import Header from '@/components/common/Header'
 import About from '@/components/views/Landing/About'
 import Description from '@/components/views/Landing/Description'
 import Event from '@/components/views/Landing/Event'
+import useScroll from '@/components/views/Landing/hooks/useScroll'
 
 import * as S from './Landing.styled'
 
 const LandingContainer = () => {
-  const scrollableDivRef = useRef<HTMLDivElement>(null)
-
-  const handleWheel = (e: WheelEvent<HTMLDivElement>) => {
-    const { deltaY } = e
-    const { scrollTop } = scrollableDivRef.current as HTMLDivElement
-    const pageHeight = window.innerHeight
-    const isScrollingDown = deltaY > 0
-    const scrollDirection = isScrollingDown ? 1 : -1
-    const nextPageTop = pageHeight * (Math.floor(scrollTop / pageHeight) + scrollDirection)
-
-    scrollableDivRef.current?.scrollTo({
-      top: nextPageTop,
-      behavior: 'smooth',
-    })
-  }
+  const { containerRef } = useScroll()
 
   return (
     <S.Layout>
       <Header />
-      <S.ScrollBlock ref={scrollableDivRef} onWheel={handleWheel}>
+      <S.ScrollBlock ref={containerRef}>
         <About />
         <Description />
         <Event />
